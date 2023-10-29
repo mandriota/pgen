9710ef3a9d19258dcb5aa0ccdbe8b2abf684cc21
Mark Mandriota <mandriotamark@gmail.com>
2023-10-29 16:33:54 +0100

    refactoring; add passing charset trought stdin support

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <getopt.h>
 #include <memory.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <assert.h>
+#include <stdio.h>
 
 #define STR_IDENT(x) #x
 #define STR_VALUE(x) STR_IDENT(x)
@@ -10,48 +11,57 @@
 #define CHARSET_LOWERS "qwertyuiopasdfghjklzxcvbnm"
 #define CHARSET_UPPERS "QWERTYUIOPASDFGHJKLZXCVBNM"
 #define CHARSET_DIGITS "0123456789"
-#define CHARSET_SPECIALS "^*+-=_?.,:;\\|/\"'$%&@#()[]{}"
-#define CHARSET_DEFAULT CHARSET_LOWERS CHARSET_UPPERS CHARSET_DIGITS
+#define CHARSET_SEQUEL "^*+-=_?.,:;\\|/\"'$%&@#()[]{}"
+#define DEFAULT_CHARSET CHARSET_LOWERS CHARSET_UPPERS CHARSET_DIGITS
 
-#define PLENGTH_DEFAULT 8
+enum {
+  BUF_LEN = 4096,
+  DEFAULT_PWD_LEN = 8,
+};
 
+size_t push(char *restrict dst, const char *src, size_t dst_off,
+            size_t dst_cap);
 size_t parse(const char *s);
-size_t append(char *restrict dst, const char *src, size_t dst_off,
-              size_t dst_cap);
-
+size_t trim_last_newline(const char * src, size_t src_len);
 void print_help();
 
 int main(int argc, char * argv[]) {
-  char result[4096];
-  size_t result_len = PLENGTH_DEFAULT;
+  char buf[BUF_LEN];
+  size_t pwd_rem = DEFAULT_PWD_LEN;
 
-  char charset[1<<sizeof(char)*8];
+  char charset[1 + (unsigned char) ~0];
   size_t charset_len = 0;
 
+  assert(BUF_LEN > sizeof charset);
+
   int opt;
-  while ((opt = getopt(argc, argv, "hn:c:luds")) != -1) {
+  while ((opt = getopt(argc, argv, "hn:c:Cluds")) != -1) {
 	switch (opt) {
 	case 'h':
 	  print_help();
 	  return 0;
 	case 'n':
-	  result_len = parse(optarg);
+	  pwd_rem = parse(optarg);
 	  break;
 	case 'c':
-	  if ((charset_len = append(charset, optarg, charset_len, sizeof charset))+1 == 0)
+	  if ((charset_len = push(charset, optarg, charset_len, sizeof charset))+1 == 0)
 		charset_len = sizeof charset;
 	  break;
+	case 'C':
+	  buf[trim_last_newline(buf, read(STDIN_FILENO, buf, sizeof charset))] = '\0';
+	  charset_len = push(charset, buf, charset_len, sizeof charset);
+	  break;
 	case 'l':
-	  charset_len = append(charset, CHARSET_LOWERS, charset_len, sizeof charset);
+	  charset_len = push(charset, CHARSET_LOWERS, charset_len, sizeof charset);
 	  break;
 	case 'u':
-	  charset_len = append(charset, CHARSET_UPPERS, charset_len, sizeof charset);
+	  charset_len = push(charset, CHARSET_UPPERS, charset_len, sizeof charset);
 	  break;
 	case 'd':
-	  charset_len = append(charset, CHARSET_DIGITS, charset_len, sizeof charset);
+	  charset_len = push(charset, CHARSET_DIGITS, charset_len, sizeof charset);
 	  break;
 	case 's':
-	  charset_len = append(charset, CHARSET_SPECIALS, charset_len, sizeof charset);
+	  charset_len = push(charset, CHARSET_SEQUEL, charset_len, sizeof charset);
 	  break;
 	case '?':
 	  return 1;
@@ -59,21 +69,21 @@ int main(int argc, char * argv[]) {
   }
 
   if (charset_len == 0)
-	charset_len = append(charset, CHARSET_DEFAULT, charset_len, sizeof charset);
+	charset_len = push(charset, DEFAULT_CHARSET, charset_len, sizeof charset);
 
   size_t i;
   sranddev();
 
   do {
-	for (i = 0; i < sizeof result && i < result_len; ++i)
-	  result[i] = charset[rand()%charset_len];
+	for (i = 0; i < sizeof buf && i < pwd_rem; ++i)
+	  buf[i] = charset[rand()%charset_len];
 
-	result_len -= i;
-	write(STDOUT_FILENO, result, i);
-  } while (result_len);
+	pwd_rem -= i;
+	write(STDOUT_FILENO, buf, i);
+  } while (pwd_rem);
 
-  result[0] = '\n';
-  write(STDOUT_FILENO, result, 1);
+  buf[0] = '\n';
+  write(STDOUT_FILENO, buf, 1);
   
   return 0;
 }
@@ -88,8 +98,14 @@ size_t parse(const char * s) {
   return r;
 }
 
-size_t append(char * restrict dst, const char * src, size_t dst_off, size_t dst_cap) {
-  return (char*) memccpy(dst+dst_off, src, '\0', dst_cap - dst_off)-dst-1;
+size_t push(char * restrict dst, const char * src, size_t dst_off, size_t dst_cap) {
+  char * ptr = memccpy(dst+dst_off, src, '\0', dst_cap - dst_off);
+  if (ptr == NULL) return dst_off;
+  return ptr-dst-1;
+}
+
+size_t trim_last_newline(const char * src, size_t src_len) {
+  return src_len-(src[src_len-1] == '\n');
 }
 
 void print_help() {
@@ -105,10 +121,12 @@ void print_help() {
   puts("\t -n <number>");
   puts("\t   password length (defaults to " STR_VALUE(PLENGTH_DEFAULT) ")");
   puts("");
-  puts("\tCharset (defaults to \"" CHARSET_DEFAULT "\"):");
+  puts("\tCharset (defaults to \"" DEFAULT_CHARSET "\"):");
   puts("");
   puts("\t -c <string>");
   puts("\t   add a custom string to charset");
+  puts("\t -C");
+  puts("\t   add a custom string to charset (read from stdin)");
   puts("\t -l");
   puts("\t   add lower letters to charset");
   puts("\t -u");
